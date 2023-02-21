c7bdf14bc8b885c6f57789909a07fb57074dcfd9
Hrexandro <marcin_nowak_hrx@o2.pl>
2023-02-21 21:51:20 +0100

    add animation and error placeholder image

--- script.js
+++ script.js
@@ -5,24 +5,34 @@ const searchButton = document.getElementById('button-search')
 
 let searchedPhrase = "skull"
 
+function clearAnimations (){
+    Array.from(document.getElementsByClassName("button")).forEach((element) => {element.classList.remove("animated")})
+}
+
 function fetchImage(){
     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=BswaVFI9nEK0O8Qm7LJzNYQ4KCgbLYlU&s=${searchedPhrase}`, {mode: 'cors'})
     .then(function(response){
         return response.json()
     })
     .then(function(response){
-        console.log(response)
         img.src = response.data.images.original.url
+        clearAnimations()
+    }) .catch((error) => {
+        console.log(error)
+        img.src = "./placeholder.png"
+        clearAnimations()
     })
 }
 
 const refreshButton = document.getElementById('button1')
 
 refreshButton.addEventListener('click', ()=>{
+    refreshButton.classList.add("animated")
     fetchImage()
 })
 
 searchButton.addEventListener('click', ()=>{
+    searchButton.classList.add("animated")
     searchedPhrase = searchBar.value
     searchBar.value = ""
     fetchImage()
